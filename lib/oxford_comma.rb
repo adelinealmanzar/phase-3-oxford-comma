36588fe78d097e56f6a0d1757f6b307ed7500e7b
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-22 09:31:01 -0800

    commit tree c37dc3a3020aed1a7cc131e65141525953cbd7f1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    new_array = []
+    if array.length == 1
+        array.join(", ")
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        # up until last el
+        i = 0
+        while i < array.length - 1 do
+            new_array << array[i]
+            i += 1
+        end
+        # last el
+        new_array << "and #{array[array.length - 1]}"
+        new_array.join(", ")
+    end
 end
